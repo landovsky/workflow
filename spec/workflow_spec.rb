2e930f4ce189ade633d4259d8514c84f06e3b38b
Tomas Landovsky <landovsky@gmail.com>
2023-02-27 18:55:30 +0100

    dir structure

---
+++ spec/workflow_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Workflow do
+  it "has a version number" do
+    expect(Workflow::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
